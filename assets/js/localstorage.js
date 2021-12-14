c3005c5b730f74e4292eaf40570f5ce2028c06bf
clintonjosephs <clintonjosephs26@gmail.com>
2021-12-14 15:39:55 +0100

    fixed linters errors

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -1,24 +1,26 @@
-let clientName = document.querySelector('#user_name');
-let clientEmail = document.querySelector('#user_email');
-let clientMessage = document.querySelector('#user_message');
+const clientName = document.querySelector('#user_name');
+const clientEmail = document.querySelector('#user_email');
+const clientMessage = document.querySelector('#user_message');
 
 function useLocalStorage() {
-    let contactFormData = {
-        name: clientName.value,
-        email: clientEmail.value,
-        message: clientMessage.value
-    };
-    localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
+  const contactFormData = {
+    name: clientName.value,
+    email: clientEmail.value,
+    message: clientMessage.value,
+  };
+  localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
 }
+
 const formInputs = document.querySelectorAll('input , textarea');
-for (let j = 0; j < formInputs.length; j+=1) {
-    formInputs[j].addEventListener('change', () => {
-        useLocalStorage();
-    });
+for (let j = 0; j < formInputs.length; j += 1) {
+  formInputs[j].addEventListener('change', () => {
+    useLocalStorage();
+  });
 }
-let formData = JSON.parse(localStorage.getItem('contactFormData'));
-clientName.value = formData.name;
-clientEmail.value = formData.email;
-clientMessage.value = formData.message;
 
-
+const formData = JSON.parse(localStorage.getItem('contactFormData'));
+if (formData !== null) {
+  clientName.value = formData.name;
+  clientEmail.value = formData.email;
+  clientMessage.value = formData.message;
+}
