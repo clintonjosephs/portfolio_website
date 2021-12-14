368d370538f8a1e6f26beaf194fd0aa0af5c544a
clintonjosephs <clintonjosephs26@gmail.com>
2021-12-14 09:24:43 +0100

    Fixed linters errors

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,29 +1,29 @@
 const form = document.querySelector('.contact-form');
-const EMAIL_LOWERCASE = "Please email should be lowercase characters *";
-
+const EMAIL_LOWERCASE = 'Please email should be lowercase characters *';
 
 function showMessage(input, message, type) {
-	const msg = input.parentNode.querySelector("span");
-	msg.innerText = message;
-	input.className = type ? "success" : "error";
-	return type;
+  const msg = input.parentNode.querySelector('span');
+  msg.innerText = message;
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
+
 function validateEmail(input, invalidMsg) {
-    const email = input.value.trim();
-    if (email !== email.toLowerCase()) {
-        return showError(input, invalidMsg);
-    }
-    return true;
+  const email = input.value.trim();
+  if (email !== email.toLowerCase()) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    let emailValid = validateEmail(form.elements["user_email"], EMAIL_LOWERCASE);
-    if (emailValid) {
-        form.submit();
-        form.reset();
-    }
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailValid = validateEmail(form.elements.user_email, EMAIL_LOWERCASE);
+  if (emailValid) {
+    form.submit();
+    form.reset();
+  }
 });
