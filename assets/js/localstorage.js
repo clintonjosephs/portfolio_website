b42c1cfca142549b88b0fb588f9e3bb92c0bc40e
mugisha Samuel <mugishasamuel42@gmail.com>
2021-12-14 06:35:44 -0800

    add localstorage event handler to input elements

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -10,3 +10,15 @@ function useLocalStorage() {
     };
     localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
 }
+const formInputs = document.querySelectorAll('input , textarea');
+for (let j = 0; j < formInputs.length; j+=1) {
+    formInputs[j].addEventListener('change', () => {
+        useLocalStorage();
+    });
+}
+let formData = JSON.parse(localStorage.getItem('contactFormData'));
+clientName.value = formData.name;
+clientEmail.value = formData.email;
+clientMessage.value = formData.message;
+
+
