6757f6eb817f5614b4b51909834a9eb17f4a0f54
Clinton Mbonu <82473943+clintonjosephs@users.noreply.github.com>
2021-12-15 14:11:46 +0100

    Bug Fix

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -6,7 +6,7 @@ function useLocalStorage() {
   const contactFormData = {
     name: clientName.value,
     email: clientEmail.value,
-    message: clientMessag.value,
+    message: clientMessage.value,
   };
   localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
 }
